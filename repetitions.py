ee5370d81d68548c129cb9814dd9f6bae0979232
Konnits <70662481+Konnits@users.noreply.github.com>
2023-03-09 11:13:15 -0300

    Repetitions accepted

--- repetitions.py
+++ repetitions.py
@@ -7,12 +7,14 @@ max_counter = 1
 
 for char in text:
     if i == n:
+        if max_counter < counter:
+            max_counter = counter
         break
-
     if char == text[i]:
         counter += 1
     else:
         if max_counter < counter:
             max_counter = counter
         counter = 1
+    i += 1
 print(max_counter)
