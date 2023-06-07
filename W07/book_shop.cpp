436c6bbb324fdcad8ea6be8faa0b729cc4a5ac63
konnits <gerald.mendez@sansano.usm.cl>
2023-06-07 18:44:22 -0400

    Mejoras

--- W07/book_shop.cpp
+++ W07/book_shop.cpp
@@ -1,52 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <numeric>
+using namespace std;
 
 int main() {
-    int n_books, max_price;
-    std::cin >> n_books >> max_price;
+    int n, x;
+    cin >> n >> x;
 
-    std::vector<int> prices(n_books), pages(n_books);
-
-    for (int i = 0; i < n_books; ++i) {
-        std::cin >> prices[i];
+    vector<int> prices(n), pages(n);
+    for (int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> pages[i];
     }
 
-    for (int i = 0; i < n_books; ++i) {
-        std::cin >> pages[i];
-    }
-
-    int total_price = std::accumulate(prices.begin(), prices.end(), 0);
-    int total_pages = std::accumulate(pages.begin(), pages.end(), 0);
-    std::vector<int> possibilities = {0};
-
-    if (total_price <= max_price) {
-        std::cout << total_pages << std::endl;
-    } else {
-        for (int i = 0; i < n_books; ++i) {
-            std::vector<int> prices_aux(prices), pages_aux(pages);
-            total_price = std::accumulate(prices_aux.begin(), prices_aux.end(), 0);
-            total_pages = std::accumulate(pages_aux.begin(), pages_aux.end(), 0);
-
-            for (int j = 0; j < n_books; ++j) {
-                if (total_price <= max_price) {
-                    int position = std::distance(pages.begin(), std::min_element(pages.begin(), pages.end()));
-                    prices.erase(prices.begin() + position);
-                    pages.erase(pages.begin() + position);
-                    possibilities.push_back(total_pages);
-                    break;
-                }
-
-                int position = std::distance(prices_aux.begin(), std::max_element(prices_aux.begin(), prices_aux.end()));
-                total_price -= prices_aux[position];
-                total_pages -= pages_aux[position];
-                prices_aux.erase(prices_aux.begin() + position);
-                pages_aux.erase(pages_aux.begin() + position);
-            }
+    vector<int> dp(x+1, 0);
+    for (int i = 0; i < n; i++) {
+        for (int j = x; j >= prices[i]; j--) {
+            dp[j] = max(dp[j], dp[j-prices[i]] + pages[i]);
         }
-        std::cout << *std::max_element(possibilities.begin(), possibilities.end()) << std::endl;
     }
+
+    cout << dp[x] << endl;
 
     return 0;
 }
